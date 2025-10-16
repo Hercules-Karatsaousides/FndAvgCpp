433422358907dd135380a92377cbae19ef524f85
Hercules_Karatsaousides <karatsaousides.e@live.unic.ac.cy>
2025-10-17 01:48:41 +0300

    Update CMake configuration and refactor main.cpp for improved portability and functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // include / import project libraries / Headers
 #include <iostream>
 #include <limits>
+#include <vector>
 #include <windows.h>
 
 
@@ -17,6 +18,15 @@ int main() {
     // Set console to UTF-8 (had problems with div symbol)
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+    // Try to enable ANSI escape codes on Windows 10+ consoles
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE) {
+        DWORD mode = 0;
+        if (GetConsoleMode(hOut, &mode)) {
+            SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        }
+    }
 
     // Char variable used to input user choice
     // 1 = to default 5 variables
@@ -85,32 +95,36 @@ int main() {
                 break;
             }
         }
-
-        // Create array to hold custom number of variables
-        double custom_table[custom];
+        
+        // Create dynamic array to hold custom number of variables (portable)
+        std::vector<double> custom_table(static_cast<size_t>(custom));
         cout<<endl;
-
+        
         // Take User input for array
         for (int i = 0; i < custom; i++) {
             cout<<"\033[97mEnter number \033[0m"<<i+1<<":\n";
             cout<<"> ";
-            input(custom_table[i]);
+            input(custom_table[static_cast<size_t>(i)]);
         }
-
+        
         // Print the average of the custom number of variables
         cout<<endl;
         cout<<"\033[97mThe average of the \033[93m"<<custom<<" \033[97mnumbers you entered is: \033[0m"<<endl;
-        cout<<custom_table[0];
-
-        // Simple for loop for calculating the sum of the numbers
-        for (int i = 1; i < custom; i++) {
-            cout<<" + "<<custom_table[i];
-            custom_average += custom_table[i];
+        
+        // Calculate and print the sum
+        double sum = 0.0;
+        for (int i = 0; i < custom; i++) {
+            if (i == 0) {
+                cout<<custom_table[0];
+            } else {
+                cout<<" + "<<custom_table[static_cast<size_t>(i)];
+            }
+            sum += custom_table[static_cast<size_t>(i)];
         }
-
+        
         // Finish printing the average in a UserFriendly format
         cout<<" ÷ "<<custom<<" = ";
-        custom_average = custom_average / custom;
+        custom_average = sum / static_cast<double>(custom);
         cout<<custom_average<<endl;
     }
 
